2e7fbdf23d14a109d1f547c53caabffc48b1fcb1
110kotaro <kotaro.ito@pathoslogos.co.jp>
2025-10-22 11:08:23 +0900

    2回目提出後の修正

--- calculator10119-app/src/app/app.component.ts
+++ calculator10119-app/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
@@ -26,6 +26,9 @@ export class AppComponent {
   justSqrt:boolean=false; //最後に√が押されたかどうかを判断するため。
   justCE:boolean=false; //最後にCEが押されたかどうかを判断するため。
   justEqAndCE:boolean=false; //最後に=とCEが押されたかどうかを判断するため。
+  justDecimalPoint:boolean=false; //小数点入力直後かどうかを判断するため。
+  justNumber:boolean=false; //数値入力直後かどうかを判断するため。
+  justClear:boolean=true; //全クリア直後かどうかを判断するため。
 
 
   append(value:string):void{
@@ -40,7 +43,7 @@ export class AppComponent {
         this.currentValue='';
       }
       if(this.currentValue===''){ //現在の値がない場合。「＝」と「CE」でcurrentValueを消しているため、やり直した計算ができるように。
-        if(this.prevValue!==''/*&&this.operator!==''*/){ //prevValueとoperatorがある場合、operatorをvalueに変更
+        if(this.prevValue!==''){ //prevValueとoperatorがある場合、operatorをvalueに変更
           this.operator=value;
         }else{
           this.prevValue='0';
@@ -52,6 +55,9 @@ export class AppComponent {
         this.justSqrt=false;
         this.justCE=false;
         this.justEqAndCE=false;
+        this.justDecimalPoint=false;
+        this.justNumber=false;
+        this.justClear=false;
         return; //なにもしない
       }
 
@@ -63,6 +69,9 @@ export class AppComponent {
         this.justSqrt=false;
         this.justCE=false;
         this.justEqAndCE=false;
+        this.justDecimalPoint=false;
+        this.justNumber=false;
+        this.justClear=false;
         return;
       }
 
@@ -98,11 +107,15 @@ export class AppComponent {
       this.justSqrt=false; //最後に演算子が押されたため戻す。
       this.justCE=false; //最後に演算子が押されたため戻す。
       this.justEqAndCE=false;
+      this.justDecimalPoint=false;
+      this.justNumber=false;
+      this.justClear=false;
+
     } //演算子入力処理ここまで。計算結果は左辺に入っている。
     
 
     if(value==='.'){ //もしvalueが小数点だった場合
-      if(this.justCalc||this.justPerCalc||this.justSqrt){//=直後の場合、クリア
+      if(this.justCalc||this.justPerCalc||this.justSqrt||this.justClear){//=直後の場合、クリア
         this.currentValue='';
         this.prevValue='';
         this.operator='';
@@ -125,12 +138,15 @@ export class AppComponent {
       this.justCalc=false; //最後に小数点が押されたため戻す。
       this.justSqrt=false; //最後に小数点が押されたため戻す。
       this.justEqAndCE=false;
+      this.justDecimalPoint=true;
+      this.justNumber=false;
+      this.justClear=false;
     } //小数点入力処理ここまで
 
     
     if(value==='+/-'){ //もしvalueが+/-だった場合。常に現在の表示を反転させる。
       if(this.currentValue===''&&this.prevValue===''){ //数値がまっさらなら
-        return; //なにもしない
+        this.currentValue='0';
       }
       if(this.justPerCalc){
         this.currentValue=''; //％直後であれば、currentをクリア。％後にcurrentを消せないため。
@@ -154,11 +170,12 @@ export class AppComponent {
       if(this.justPerCalc){//最後に%が押された場合
         if(this.justSqrt){ //%√後、falseにする。
           if(this.operator==='/'){
-            this.operator='*';
+            this.justPerCalc=false;
+          }else{
+            this.justPerCalc=false;
+            this.currentValue=this.prevValue; //計算結果を右辺とする。
+            this.prevValue=this.lastResult; //元の左辺代入。
           }
-          this.justPerCalc=false;
-          this.currentValue=this.prevValue; //計算結果を右辺とする。
-          this.prevValue=this.lastResult; //元の左辺代入。
         }else
         if(this.operator==='+'||this.operator==='-'){ //+/-直後の場合、なにもしない。
           return;
@@ -181,6 +198,9 @@ export class AppComponent {
         this.lastResult=this.prevValue; //％直後でも＝直後でもない場合、左辺をlastResultに保存。
       }
 
+      if(this.justCE){
+        this.operator=this.lastOperator; //演算子numCEnumで％が反応しなかったので、演算子を復活させる。
+      }
       if(this.prevValue===''||this.operator===''){
         return; //左辺がない、または演算子もない場合、なにもしない。
       }
@@ -211,6 +231,9 @@ export class AppComponent {
       this.justSqrt=false; //最後に%が押されたため戻す。
       this.justCE=false; //最後に%が押されたため戻す。
       this.justEqAndCE=false;
+      this.justDecimalPoint=false;
+      this.justNumber=false;
+      this.justClear=false;
     }//%処理ここまで　結果を左辺に入れてみた、明日要確認
 
 
@@ -223,13 +246,16 @@ export class AppComponent {
           this.subdisplay='E'; //左にEを表示
           return;
         }
-        this.display=this.prevValue;
+        this.display=this.formatDisplay(this.prevValue); //これやっとかないと表示がおかしくなるぞ.リターンしてるから
        
 
         this.justOpe=false; //最後に√が押されたため戻す。
         this.justPerCalc=true; //%√後は%後の処理と一緒にする。
         this.justCalc=false; //最後に√が押されたため戻す。
         this.justSqrt=true; //最後に√が押されたという判断。
+        this.justDecimalPoint=false;
+        this.justNumber=false;
+        this.justClear=false;
         return;
       }
 
@@ -270,12 +296,25 @@ export class AppComponent {
       this.justSqrt=true; //最後に√が押されたという判断。
       this.justCE=false; //最後に√が押されたため戻す。
       this.justEqAndCE=false;
+      this.justDecimalPoint=false;
+      this.justNumber=false;
+      this.justClear=false;
     } //√処理ここまで。
 
 
     if(value==='='){
 
       if(this.prevValue===''&&this.currentValue===''){ //左辺も右辺もない場合、「＝」が成り立たないのでなにもしない。
+        return;
+      }
+      if(this.prevValue===''&&(this.justDecimalPoint||Number(this.currentValue)===0)&&this.operator===''){
+        this.currentValue=String(parseFloat(this.currentValue));
+        
+        this.justDecimalPoint=false;
+        this.justNumber=false;
+        this.justCalc=true;
+
+        this.display=this.formatDisplay(this.currentValue);
         return;
       }
 
@@ -286,9 +325,14 @@ export class AppComponent {
       if(this.operator===''){ //=num=時
         if(this.justCE&&!this.justEqAndCE){ //演算途中のCE後（1+2*3CE5=）
           this.operator=this.lastOperator;
-        }else{
+        }else if(this.justNumber){
         this.operator=this.lastOperator;
           this.prevValue=this.currentValue;
+          this.currentValue=this.lastValue;
+        }else if(this.justSqrt){
+          this.operator=this.lastOperator;
+        }else{
+          this.operator=this.lastOperator;
           this.currentValue=this.lastValue;
         }
         
@@ -318,7 +362,7 @@ export class AppComponent {
               this.lastResult=String(lastNumber*(-1));
               this.lastValue=this.prevValue; //演算子計算結果を保存。(次右辺として再使用)
               this.prevValue=this.lastResult; //反転した値を左辺に。
-              this.display=this.prevValue;
+              this.display=this.formatDisplay(this.prevValue); //これやっとかないと表示がおかしくなるぞ
 
               this.justOpe=false; //最後に-が押されたため戻す。
               this.justPerCalc=false; //最後に-が押されたため戻す。???
@@ -326,6 +370,9 @@ export class AppComponent {
               this.justSqrt=false; //最後に-が押されたため戻す。
               this.justCE=false; //最後に-が押されたため戻す。
               this.justEqAndCE=false;
+              this.justDecimalPoint=false;
+              this.justNumber=false;
+              this.justClear=false;
               return; //脱出
             }else{
               this.currentValue=this.lastValue;
@@ -334,7 +381,7 @@ export class AppComponent {
         }
       }
 
-      
+          
 
       this.lastResult=this.prevValue; //元の左辺を一時的に保存（計算前に左辺を逃がして置けば結果が上塗りされない）
 
@@ -345,6 +392,14 @@ export class AppComponent {
         this.subdisplay='E'; //左にEを表示
         return;
       }else if(calcResult===''){ //計算結果が出ない場合、表示はそのまま。
+        this.justOpe=false; //最後に=が押されたため戻す。
+        this.justPerCalc=false; //最後に=が押されたため戻す。???
+        this.justCalc=true; //「＝」直後かどうかを判断するため。
+        this.justSqrt=false; //最後に=が押されたため戻す。
+        this.justCE=false; //最後に=が押されたため戻す。
+        this.justDecimalPoint=false;
+        this.justNumber=false;
+        this.justClear=true;
         return;
       }
       
@@ -360,11 +415,14 @@ export class AppComponent {
       this.justCalc=true; //「＝」直後かどうかを判断するため。
       this.justSqrt=false; //最後に=が押されたため戻す。
       this.justCE=false; //最後に=が押されたため戻す。
+      this.justDecimalPoint=false;
+      this.justNumber=false;
+      this.justClear=false;
     } //=処理ここまで。計算結果は左辺に入っている。
     
     
     if(this.isNumber(value)){ //もしvalueが数値だった場合
-      if(this.justCalc||this.justPerCalc||this.justSqrt){ //＝直後または%直後の場合、全クリア
+      if(this.justCalc||this.justPerCalc||this.justSqrt||this.justClear){ //＝直後または%直後の場合、全クリア
         this.currentValue='';
         this.prevValue='';
         this.operator='';
@@ -372,15 +430,45 @@ export class AppComponent {
       if(this.currentValue==='0'){ //現在の値が0の場合、0を消す
         this.currentValue='';
       }
-      if(this.currentValue.length<10){ //現在の値の長さが10未満の場合、現在の値にvalueを追加
-        this.currentValue+=value;
-        this.display=this.currentValue;
+      if(this.currentValue==='-0'){
+        this.currentValue='-';
       }
+
+      let isNegative=false;
+      if(Number(this.currentValue)<=0&&this.currentValue.includes('-')){//指数表記で数値を入力することはないはず。
+        this.currentValue=this.currentValue.slice(1); //「ー」を一旦消す。
+        isNegative=true;
+      }
+      
+      if(this.currentValue.includes('.')){ //小数点がある場合
+        let i=this.currentValue.indexOf('.');
+        if(i===1){ //整数部が一桁の場合、<11にすると、小数点以下第9位までいくので、10にする。
+          if(this.currentValue.length<10){ //現在の値の長さが10未満の場合、現在の値にvalueを追加
+            this.currentValue+=value;
+          }
+        }else{
+          if(this.currentValue.length<11){ //現在の値の長さが11未満の場合、現在の値にvalueを追加
+            this.currentValue+=value;
+          }
+        }
+      }else{
+        if(this.currentValue.length<10){ //現在の値の長さが10未満の場合、現在の値にvalueを追加
+          this.currentValue+=value;
+        }
+      }
+      if(isNegative){
+        this.currentValue='-'+this.currentValue;
+      }
+
+      this.display=this.currentValue;
 
       this.justOpe=false; //最後に数字が押されたため戻す。
       this.justPerCalc=false; //最後に数字が押されたため戻す。???
       this.justCalc=false; //数字が押されたため戻す。
       this.justSqrt=false; //最後に数字が押されたため戻す。
+      this.justDecimalPoint=false;
+      this.justNumber=true;
+      this.justClear=false;
 
     } //数値処理ここまで
 
@@ -410,6 +498,9 @@ export class AppComponent {
     this.justSqrt=false;
     this.justCE=false;
     this.justEqAndCE=false;
+    this.justDecimalPoint=false;
+    this.justNumber=false;
+    this.justClear=true; 
   }
 
   clearentry(value:string){ //直近の数字をキャンセル、表示を０。
@@ -418,6 +509,7 @@ export class AppComponent {
       this.prevValue=this.display.replace(/'/g,'');
       this.currentValue='';
       this.operator='';
+      this.justNumber=false;
       return;
     }
     if(this.justSqrt){
@@ -425,17 +517,27 @@ export class AppComponent {
       this.lastValue='';
       this.lastResult='';
     }
+
+    if(this.justOpe){
+      this.currentValue='';
+      this.justCE=true;
+      this.justNumber=false;
+      return;
+    }
     this.currentValue='0';
     this.lastOperator=this.operator;
     this.operator='';
 
     if(this.justCalc){
       this.justEqAndCE=true;
+    }else if(this.justPerCalc){
+      this.justCE=true;
     }else{
       this.display='0';
     }
 
     this.justCE=true;
+    this.justNumber=false;
   }
 
 
@@ -450,45 +552,76 @@ export class AppComponent {
   }
 
   formatDisplay(value:string):string{ //表示を3桁ごとにカンマを入れる関数
+    
+    if(value.includes('e')){ //指数表記の際に、小数点以下の桁数を調整する。
+      value=this.formatExp(value);
+    }
+
     const [integer,decimal] = value.split('.');
     const formatInt=integer.replace(/\B(?=(\d{3})+(?!\d))/g,'\'');
-    return decimal ? `${formatInt}.${decimal}` : formatInt;
+    if(this.justDecimalPoint){ //小数点入力直後の場合、小数点を表示。
+      return decimal ? `${formatInt}.${decimal}` : `${formatInt}.`;
+    }else{
+      return decimal ? `${formatInt}.${decimal}` : formatInt;
+    }
   }
 
   formatResult(value:string):string{ //計算結果の桁数制限を行う関数
     let rv=value;
-    let r=Number(value);
 
-    if(rv.includes('-')){
-      if(rv.includes('.')){
-        const i=rv.indexOf('.');
-        rv=String(r.toFixed(10-i).replace(/\.?0+$/,''));
+    let isNegative=false;
+    if(Number(rv)<0){ //負の場合、「ー」を一旦消す。
+      isNegative=true;
+      rv=rv.slice(1);
+    }
+
+    let r=Number(rv);
+
+    if(rv.includes('e')){
+      rv=this.formatExp(rv);
+    }
+
+    if(rv.includes('.')){ //小数の場合
+      const i=rv.indexOf('.');
+      if(i>9){ //整数部分10桁以上の場合。合計12個以上
+        r=Math.round(r)
+        rv=String(r);
+        //合計10個になり、小数点消える。
+      }else if(i===1){
+        const tenExp=Math.pow(10,8);
+        r=Math.round((r+Number.EPSILON)*tenExp)/tenExp; //整数に直してから四捨五入して、小数に戻す。
+        rv=String(r);
+        //整数部が一桁の時、小数点以下8位までとなるように。
+      }else{ //整数部分9桁以内の場合。最大合計11個
+        const tenExp=Math.pow(10,10-i);
+        r=Math.round((r+Number.EPSILON)*tenExp)/tenExp; //整数に直してから四捨五入して、小数に戻す。
+        rv=String(r);
+       //999999999.9、999999999.1。小数点以下9位まで計算。
       }
-      if(rv.length>11){
-        this.subdisplay='E'; //左にEを表示
-        const i=rv.length;
-        const integer=rv.slice(0,i-11);
-        const decimal=rv.slice(i-11);
-        rv=integer+'.'+decimal;
-        return rv=rv.slice(0,11); //修正：Eを消して、桁数制限処理のみ
+      if(rv.includes('.')){ //まだ小数点が残っている場合のみ、無駄な0を削除。
+        rv=rv.replace(/\.?0+$/,''); //四捨五入処理したものを、指数回避＆整形
+      }
+    } //ここまでで、小数点の処理が終わって、整数部分が10桁以内の場合の処理が終わっている。
+
+    if(rv.length>10&&!rv.includes('.')){ //整数部分が11桁以上の場合
+      this.subdisplay='E'; //左にEを表示
+      const i=rv.length;
+      const integer=rv.slice(0,i-10); //桁数超えた分を切り取る。
+      const decimal=rv.slice(i-10); //超えてない部分を切り取る。
+      rv=integer+'.'+decimal;
+      if(integer.length===1){
+        rv=rv.slice(0,10); //整数部が1桁の場合、小数点込みで10文字となるように。
       }else{
-        return rv;
+        rv=rv.slice(0,11); //整数部が2桁以上の場合、小数点込みで11文字となるように。
       }
-    }else{
-      if(rv.includes('.')){
-        const i=rv.indexOf('.');
-        rv=String(r.toFixed(9-i).replace(/\.?0+$/,''));
-      }
-      if(rv.length>10){
-        this.subdisplay='E'; //左にEを表示
-        const i=rv.length;
-        const integer=rv.slice(0,i-10);
-        const decimal=rv.slice(i-10);
-        rv=integer+'.'+decimal;
-        return rv=rv.slice(0,10); //修正：Eを消して、桁数制限処理のみ
-      }else{
-        return rv;
-      }
+    }
+
+    if(isNegative){
+      rv='-'+rv;
+    }
+
+    if(rv==='-0'){
+      rv='0';
     }
     return rv;
   }
@@ -520,7 +653,9 @@ export class AppComponent {
 
   toggle(value:string):string{ //+/-を押した時に、現在の値を反転させる関数
     let toggleValue=value;
-    if(toggleValue.includes('-')){
+    if(Number(toggleValue)<0){ //includes('-')だと、指数表記でバグる。
+      toggleValue=toggleValue.slice(1);
+    }else if(Number(toggleValue)===0&&toggleValue.includes('-')){
       toggleValue=toggleValue.slice(1);
     }else{
       toggleValue='-'+toggleValue;
@@ -563,24 +698,45 @@ export class AppComponent {
     let sqrtResult:number=0;
     const valueNumber=Number(value);
    
-    if(value.includes('-')){ //負の値を√しようとしたらできないよ
+    if(Number(value)<0){ //負の値を√しようとしたらできないよ
       return value='';
     }
 
     sqrtResult=Math.sqrt(valueNumber)//平方根を計算。例外は、数字が打たれていない時、
     resultValue=String(sqrtResult);
 
-      if(resultValue.includes('.')){
-        const i=resultValue.indexOf('.');
-        resultValue=String(sqrtResult.toFixed(9-i).replace(/\.?0+$/,''));
-      }
-      if(resultValue.length>10){
-        resultValue=resultValue.slice(0,10);
-      }
+    resultValue=this.formatResult(resultValue);
+   
       return resultValue;
     }
 
-  
+  formatExp(value:string):string{ //指数表記を無理やり展開する関数
+    let isNegative=false;
+    if(value.startsWith('-')){
+      value=value.slice(1);
+      isNegative=true;
+    }
+
+    let [base,exponent] = value.split('e'); //eを基準に二つに分ける（2e-8→2, -8）
+    const expNumber=Number(exponent); //指数部を数値に変換（-8）
+
+    const [int, dec='']=base.split('.'); //数字の部分を小数点を基準に分ける（2→2,''）
+    const digits=int+dec; //つまり小数点を抜く作業。
+    
+    if(expNumber<0){ //指数部が負の場合（小数）
+      const addZero=Math.abs(expNumber)-int.length; //0を追加する数を計算。
+      const zeros=addZero>0?'0'.repeat(addZero):'';
+      value='0.'+zeros+digits; //必要な数だけ0を追加して、整理。
+    }else{ //指数部が正の場合、整数部に0を追加する。
+      const addZero=expNumber-dec.length;
+      const zeros=addZero>0?'0'.repeat(addZero):'';
+      value=digits+zeros;
+    }
+    if(isNegative){
+      value='-'+value;
+    }
+  return value;
+  }
 
  }
   
